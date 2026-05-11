2ef24aee45cc9e216fb96aa255425f9dff47b5eb
Tonyfidele <vieuxk091@gmail.com>
2026-05-11 14:22:46 +0200

    fix prisma import/export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -15,6 +15,22 @@
 // }
 
 // export default prisma;
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-export default prisma;
+//import { PrismaClient } from '@prisma/client';
+
+//const prisma = new PrismaClient();
+//export default prisma;
+
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
